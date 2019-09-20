e62fb0ea528a89ba7d7c54c3c79540c1496d3f4b
SanjeevURao <sanjeevurao@gmail.com>
2019-09-20 23:03:55 +0530

    Use Services to fetch Hero Data Asynchronously

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from "../hero";
-import { HEROES } from '../mock-heroes';
+import {HeroService} from '../hero.service';
+import { Observable, of } from 'rxjs';
+import {MessageService} from '../message.service'
 
 @Component({
   selector: 'app-heroes',
@@ -8,16 +10,33 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  constructor(private heroService : HeroService) { }
 
-  heroes = HEROES;
+  messageService : MessageService;
+  
+  heroes;
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(
+
+      heroes => this.heroes = heroes
+
+    );
+  }
+
+
 
   selectedHero: Hero;
   onSelect(hero: Hero): void {
   this.selectedHero = hero;
 }
-  constructor() { }
+ 
+  
 
   ngOnInit() {
+    
+    this.getHeroes();
+
   }
 
 }
